dd7f460286b3dbfb49a677419a133f994f1022f9
yogimardilah <yogimardilah19@gmail.com>
2021-09-30 11:41:17 +0700

    buat aktif state

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,16 +7,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link{{ request()->is('/') ? ' active' : ''}}" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
+            <a class="nav-link{{ request()->is('about') ? ' active' : ''}}" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
+            <a class="nav-link{{ request()->is('contact') ? ' active' : ''}}" href="/contact">Contact</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/login">Login</a>
+            <a class="nav-link{{ request()->is('login') ? ' active' : ''}}" href="/login">Login</a>
           </li>
        
         </ul>
