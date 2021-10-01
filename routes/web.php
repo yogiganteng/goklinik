ecb9b58b59b13cac89fe938d92db17f121f8e8c4
yogimardilah <yogimardilah19@gmail.com>
2021-10-01 19:25:45 +0700

    route Controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+
+Route::get('/', 'HomeController@index');
+
+
+
+
 
 Route::view('contact', 'contact');
 Route::view('about', 'about');
