5608b341b36878ad2df50cb12ff57c826eb82ce7
yogimardilah <yogimardilah19@gmail.com>
2021-10-04 16:34:34 +0700

    route wildcard - buat slug di url

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+
+@section('Title', 'Post')
+@section('content')
+
+
+    <div class="container">
+        <p>{{ $slug }} </p>
+    </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', 'HomeController@index');
 
 
 
-
+Route::get('post/{slug}', 'PostController@show');
 
 Route::view('contact', 'contact');
 Route::view('about', 'about');
